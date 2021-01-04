3ebf3328e7949f12af49c52d4c17a64d8fbd3f8e
ziwei531 <59821731+ziwei531@users.noreply.github.com>
2021-01-05 02:39:52 +0800

    changes

--- Final Copy of Assignment 3/delete-data-page.js
+++
@@ -1,7 +0,0 @@
-function deleteConfirm(id) {
-    const response = confirm(`Confirm deleting book record #${id}?`);
-
-    if(response) {
-        document.location.href = "delete-data.php?id=" + id;
-    }
-}
--- Final Copy of Assignment 3/log-out.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION["log_in_user"]);
-    session_destroy();
-
-    header("location: log-in-page.php");
-    exit;
-?>
